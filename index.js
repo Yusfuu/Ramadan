5af2054c7f7f98f7a960e539483da547bdf18da9
Youssef Hajjari <youssefhajjari01@gmail.com>
2021-04-17 16:59:22 +0000

    add remaining time of maghrib adan start

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 #!/usr/bin/env node
 
 const r = require("./Ramadan.json");
-const Table = require('cli-table3');
-const chalk = require('chalk');
-const meow = require('meow');
+const Table = require("cli-table3");
+const meow = require("meow");
+const _date = new Date();
+const { flags } = meow({ flags: { all: { type: "boolean", alias: "a" }, fotor: { type: "boolean", alias: "f" } } });
 
-const { flags } = meow({
-  flags: {
-    all: {
-      type: 'boolean',
-      alias: 'a'
-    }
-  }
-});
+const c = {
+  magenta: "\x1B[95m$\x1B[39m", green: "\x1B[32m$\x1B[39m", cyan: "\x1B[36m$\x1B[39m"
+}
+function getMaghrib() {
+  const t = new Date(`2021-01-01T${info.Maghrib}`);
+  const _t = new Date(`2021-01-01T${_date.toLocaleTimeString().slice(0, 5)}`);
+  const r = (t - _t) / 60000;
+  return r <= 0 ? 'Fotor Mubarak' : `${(~~(r / 60))} hour(s) and ${(r % 60)} minute(s).`;
+}
 
-const today = new Date().toDateString().match(/\w+\s\d{1,2}/gi) + "";
+const today = _date.toDateString().match(/\w+\s\d{1,2}/gi) + "";
 const info = r.find(({ date }) => date === today);
-const table = new Table({ head: ["", ...[...Object.keys(info).splice(2)].map(_ => _ == 'Maghrib' ? chalk.green(_) : chalk.cyan(_))] });
+const table = new Table({ head: ["", ...[...Object.keys(info).splice(2)].map(_ => _ == "Maghrib" ? c.green.replace("$", _) : c.cyan.replace("$", _))] });
+
+
+if (flags.fotor) {
+  console.log(c.cyan.replace('$', getMaghrib()));
+  return;
+}
 
 if (flags.all) {
   for (const key of r) {
     table.push(
-      { [chalk.magentaBright(`#${key.id}`)]: Object.values(key).splice(2).map(_ => `${_} AM`) }
+      { [c.magenta.replace("$", `#${key.id}`)]: Object.values(key).splice(2).map(_ => `${_} AM`) }
     );
   }
 } else {
   table.push(
-    { [chalk.magentaBright(`#${info.id - 1}`)]: Object.values(info).splice(2).map(_ => `${_} AM`) }
+    { [c.magenta.replace("$", `#${info.id}`)]: Object.values(info).splice(2).map(_ => `${_} AM`) }
   );
 }
 
