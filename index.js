598339ae0a69d6e574180f2c695c9341f167e01f
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-04-01 15:06:02 +0000

    new ramadany version

--- index.js
+++ index.js
@@ -1,41 +1,96 @@
 #!/usr/bin/env node
 
-const r = require("./Ramadan.json");
-const Table = require("cli-table3");
-const meow = require("meow");
-const _date = new Date();
-const { flags } = meow({ flags: { all: { type: "boolean", alias: "a" }, fotor: { type: "boolean", alias: "f" } } });
+import Table from 'cli-table3';
+import meow from 'meow';
+import chalkAnimation from 'chalk-animation';
+import { data } from './ramadan.js';
+import gradient from 'gradient-string';
+import figlet from 'figlet';
+
+const { flags } = meow({
+  flags: {
+    all: { type: 'boolean', alias: 'a' },
+    fotor: { type: 'boolean', alias: 'f' },
+  },
+});
 
 const c = {
-  magenta: "\x1B[95m$\x1B[39m", green: "\x1B[32m$\x1B[39m", cyan: "\x1B[36m$\x1B[39m"
-}
-function getMaghrib() {
-  const t = new Date(`2021-01-01T${info.Maghrib}`);
-  const _t = new Date(`2021-01-01T${_date.toLocaleTimeString().slice(0, 5)}`);
+  magenta: '\x1B[95m$\x1B[39m',
+  green: '\x1B[32m$\x1B[39m',
+  cyan: '\x1B[36m$\x1B[39m',
+};
+
+const event = new Date();
+const sleep = (ms = 5000) => new Promise((r) => setTimeout(r, ms));
+
+const today = event.toLocaleDateString('en-US', {
+  month: 'short',
+  day: 'numeric',
+});
+
+const current = event.toLocaleString('en-US', {
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
+
+const remaining = (adan) => {
+  const t = new Date(`2022-01-01T${adan.Maghrib}`);
+  const _t = new Date(`2022-01-01T${current}`);
   const r = (t - _t) / 60000;
-  return r <= 0 ? 'Fotor Mubarak' : `${(~~(r / 60))} hour(s) and ${(r % 60)} minute(s).`;
+  return r <= 0
+    ? 'Fotor Mubarak 🚀'
+    : `${~~(r / 60)} hour(s) and ${r % 60} minute(s).`;
+};
+
+// get current adan
+const adans = data.find(({ date }) => date === today);
+
+// get remaining time to maghrib
+const maghrib = remaining(adans);
+
+// create table
+const table = new Table({
+  head: ['', 'Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'],
+  style: {
+    head: ['cyan'],
+  },
+});
+
+// get all adans times
+const allAdans = () => {
+  for (const key of r) {
+    const fields = Object.values(key).splice(2);
+    const row = {
+      [adans.id]: fields,
+    };
+    table.push(row);
+  }
+  console.log(table.toString());
+};
+
+// get current adan times
+const currentAdan = () => {
+  const fields = Object.values(adans).splice(2);
+  const row = {
+    [adans.id]: fields,
+  };
+  table.push(row);
+  console.log(table.toString());
+};
+
+async function main() {
+  console.clear();
+  figlet(`Ramadany`, async (err, data) => {
+    console.log(gradient.pastel.multiline(data) + '\n');
+    const clear = chalkAnimation.rainbow(maghrib);
+    await sleep();
+    clear.stop();
+  });
 }
-
-const today = _date.toDateString().match(/\w+\s\d{1,2}/gi) + "";
-const info = r.find(({ date }) => date === today);
-const table = new Table({ head: ["", ...[...Object.keys(info).splice(2)].map(_ => _ == "Maghrib" ? c.green.replace("$", _) : c.cyan.replace("$", _))] });
-
 
 if (flags.fotor) {
-  console.log(c.cyan.replace('$', getMaghrib()));
-  return;
-}
-
-if (flags.all) {
-  for (const key of r) {
-    table.push(
-      { [c.magenta.replace("$", `#${key.id}`)]: Object.values(key).splice(2).map(_ => `${_} AM`) }
-    );
-  }
+  main();
 } else {
-  table.push(
-    { [c.magenta.replace("$", `#${info.id}`)]: Object.values(info).splice(2).map(_ => `${_} AM`) }
-  );
+  flags.all ? allAdans() : currentAdan();
 }
-
-console.log(table.toString());
